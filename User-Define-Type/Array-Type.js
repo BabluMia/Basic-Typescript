3ffaac7add46d782bdb7f1599cfb24c465ab3da3
Bablu Mia <bablumiah2000@gmail.com>
2022-07-03 22:29:12 +0600

    array type

---
+++ User-Define-Type/Array-Type.js
@@ -0,0 +1,7 @@
+// array
+// let names:string[] =["aziz" , "Rana" , "Roni"]
+var names = ["aziz", "Rana", "Roni"];
+var a = names[0], b = names[1], c = names[2];
+console.log(a);
+// multiple data in a array 
+var multipleData = ["Afif", 5, "Nidhi", 'Aziz', 44, 22];
---
+++ User-Define-Type/Array-Type.ts
@@ -0,0 +1,11 @@
+// array
+
+// let names:string[] =["aziz" , "Rana" , "Roni"]
+let names:Array<string> =["aziz" , "Rana" , "Roni"]
+
+const [a,b,c] = names
+console.log(a)
+
+// multiple data in a array 
+
+const multipleData:(string | number)[] = ["Afif",5,"Nidhi",'Aziz',44,22]
