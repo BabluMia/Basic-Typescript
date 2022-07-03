3290dba874b3ff488b7cca9052517869a58f3215
Bablu Mia <bablumiah2000@gmail.com>
2022-07-03 20:08:21 +0600

    data type

--- First-TS/index.js
+++ First-TS/index.js
@@ -1,4 +1,8 @@
 var userName = "Bablu";
-console.log(userName);
+// console.log(userName)
 var age = 23;
-console.log(age);
+// console.log(age)
+var add = function (a, b) {
+    console.log(a + b);
+};
+add(5, 6);
--- First-TS/index.ts
+++ First-TS/index.ts
@@ -1,4 +1,9 @@
 let userName = "Bablu"
-console.log(userName)
+// console.log(userName)
 let age = 23;
-console.log(age)
+// console.log(age)
+
+let add = function(a:number , b:number):void{
+   console.log(a+b)
+}
+add(5,6)
